3830cced7009bb2136e7fdd940819da6522632df
GGorAA <ggoraa1029@gmail.com>
2023-02-15 22:48:56 +0100

    improved player audio/video sync with dynamic frame limiter

--- player.lua
+++ player.lua
@@ -3,25 +3,31 @@ local function delay(time)
     while (currentTime + time > getTime()) do end
 end
 
-local function init()
-    lcd.clear()
-    playFile("/SOUNDS/badapple.wav")
-end
-
 local function run(event, touchState)
     lcd.clear()
+    local framerate_time = getTime()
+    local dynamic_frame_limiter_offset = 0
+    playFile("/SOUNDS/badapple.wav")
+
     for i, frames in ipairs(video_data) do
         local time = getTime()
+
+        lcd.drawText(0, 0, "FPS:")
+        local time_delta = getTime() - framerate_time
+        print("time_delta: ", time_delta)
+        lcd.drawText(0, 10, string.format("%0.1f", 1 / time_delta * 100))
+        dynamic_frame_limiter_offset = math.min((10 - time_delta) / 1, -0.1)
+        print("dynamic_frame_limiter_offset: ", dynamic_frame_limiter_offset)
+        framerate_time = getTime()
+
         for _, coords in ipairs(frames) do
-            -- print(string.format("Rectangle - x1: %d, x2: %d, y1: %d, y2: %d", coords[2], coords[1], coords[4], coords[3]))
-            lcd.drawFilledRectangle(coords[2], coords[1], coords[4], coords[3])
+            lcd.drawPoint(coords[2] + 22, coords[1])
         end
-        print("frame: ", i)
-        -- lcd.drawText(0, 0, string.format("%d", i))
         lcd.refresh()
-        while (time + 10 > getTime()) do end
+        print("frame limiter delay: ", 10 - dynamic_frame_limiter_offset)
+        while (time + (10 + dynamic_frame_limiter_offset) > getTime()) do end
     end
     return 0
 end
 
-return { run=run, init=init }
+return { run=run }
